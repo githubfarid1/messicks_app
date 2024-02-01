9b6a78aa6eeadfd38a6efd35bbde209a13de7a65
Mohamad Farid Saleh <github.farid1@gmail.com>
2024-02-01 22:50:51 +0900

    update scripts

--- main.py
+++ main.py
@@ -106,6 +106,8 @@ class MainFrame(ttk.Frame):
 		messicksButton = FrameButton(self, window, text="Download PDF Diagram by Text Input", class_frame=MessickPdfDownloadFrame)
 		extractButton = FrameButton(self, window, text="Extract PDF Diagram", class_frame=ExtractPdfFrame)
 		graburlButton = FrameButton(self, window, text="Grab URLs", class_frame=GrabUrlsFrame)
+		graburlVendorButton = FrameButton(self, window, text="Grab URLs By Vendor", class_frame=GrabUrlsVendorFrame)
+
 		pdfDownloadButton = FrameButton(self, window, text="Download PDF Diagram by File input", class_frame=MessickPdfDownload2Frame)
 
 		# # layout
@@ -113,7 +115,8 @@ class MainFrame(ttk.Frame):
 		messicksButton.grid(column = 0, row = 1, sticky=(W, E, N, S), padx=15, pady=5, columnspan=3)
 		extractButton.grid(column = 0, row = 2, sticky=(W, E, N, S), padx=15, pady=5, columnspan=3)
 		graburlButton.grid(column = 0, row = 3, sticky=(W, E, N, S), padx=15, pady=5, columnspan=3)
-		pdfDownloadButton.grid(column = 0, row = 4, sticky=(W, E, N, S), padx=15, pady=5, columnspan=3)
+		graburlVendorButton.grid(column = 0, row = 4, sticky=(W, E, N, S), padx=15, pady=5, columnspan=3)
+		pdfDownloadButton.grid(column = 0, row = 5, sticky=(W, E, N, S), padx=15, pady=5, columnspan=3)
 
 
 class MessickPdfDownloadFrame(ttk.Frame):
@@ -211,6 +214,36 @@ class GrabUrlsFrame(ttk.Frame):
 	def run_process(self, **kwargs):
 			# messagebox.showwarning(title='Warning', message='')
 			run_module(comlist=[PYLOC, "modules/urlsmessick.py"])
+
+class GrabUrlsVendorFrame(ttk.Frame):
+	def __init__(self, window) -> None:
+		super().__init__(window)
+		# configure
+		self.grid(column=0, row=0, sticky=(N, E, W, S), columnspan=4)
+		self.config(padding="20 20 20 20", borderwidth=1, relief='groove')
+
+		self.columnconfigure(0, weight=1)
+		self.rowconfigure(0, weight=1)
+		self.rowconfigure(1, weight=1)
+		self.rowconfigure(2, weight=1)
+		self.rowconfigure(3, weight=1)
+		self.rowconfigure(4, weight=1)
+		self.rowconfigure(5, weight=1)
+		
+		# populate
+		titleLabel = TitleLabel(self, text="Grab URLs By Vendor")
+		closeButton = CloseButton(self)
+
+		runButton = ttk.Button(self, text='Run Process', command = lambda:self.run_process())
+		
+		# layout
+		titleLabel.grid(column = 0, row = 0, sticky = (W, E, N, S))
+		runButton.grid(column = 0, row = 5, sticky = (E))
+		closeButton.grid(column = 0, row = 6, sticky = (E, N, S))
+
+	def run_process(self, **kwargs):
+			# messagebox.showwarning(title='Warning', message='')
+			run_module(comlist=[PYLOC, "modules/urlsmessick_vendor.py"])
 
 class MessickPdfDownload2Frame(ttk.Frame):
 	def __init__(self, window) -> None:
